475bd7aa652f4e21370db43b812dafe9596cbae2
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-09-14 13:35:13 +0300

    import theme component

---
+++ design/themes/base-theme/circular-font/circular-font.compositions.tsx
@@ -0,0 +1,88 @@
+// import React from 'react';
+// import { circularFont } from './index';
+
+export {};
+
+// export function FrontsPreview() {
+//   return (
+//     <div className={circularFont} style={{ padding: 8 }}>
+//       <div style={{ fontWeight: 300 }}>
+//         light (300)
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div>
+//         regular (400)
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontWeight: 500 }}>
+//         medium (500)
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontWeight: 'bold' }}>
+//         bold (700)
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontWeight: 800 }}>
+//         extra-bold (800)
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontWeight: 900 }}>
+//         black (900)
+//         <TestText />
+//       </div>
+
+//       <br />
+
+//       <div style={{ fontWeight: 300, fontStyle: 'italic' }}>
+//         light (300)
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontStyle: 'italic' }}>
+//         regular (400) + italic
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontWeight: 500, fontStyle: 'italic' }}>
+//         medium (500)
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontStyle: 'italic', fontWeight: 'bold' }}>
+//         bold (700) + italic
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontStyle: 'italic', fontWeight: 800 }}>
+//         ultra-bold (800) + italic
+//         <TestText />
+//       </div>
+//       <Sep />
+//       <div style={{ fontStyle: 'italic', fontWeight: 900 }}>
+//         black (900) + italic
+//         <TestText />
+//       </div>
+//     </div>
+//   );
+// }
+
+// function TestText() {
+//   return (
+//     <div>
+//       abcdefghijklmnopqrstuvwxyz
+//       <br />
+//       ABCDEFGHIJKLMNOPQRSTUVWXYZ
+//       <br />
+//       0123456789
+//     </div>
+//   );
+// }
+
+// function Sep() {
+//   return <div style={{ height: '0.5em' }} />;
+// }
---
+++ design/themes/base-theme/circular-font/index.ts
@@ -0,0 +1,3 @@
+import styles from './circular-font.module.scss';
+
+export const { circularFont } = styles;
